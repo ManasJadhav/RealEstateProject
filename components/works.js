790135d631fee8d27e13b49875e41e887c1f60ff
manasjadhav <manasjjadhav@gmail.com>
2022-08-12 19:27:15 +0530

    changed the how it works..

--- components/works.js
+++ components/works.js
@@ -16,7 +16,7 @@ const Works = () => {
             <div className="bg-[#072960] text-white px-6 py-4 lg:h-full h-1/2 rounded-full mx-4 ">
               <h1 className="font-bold">1</h1>
             </div>
-            <div className="text-blue-900 font-semibold px-4 ">
+            <div className="text-blue-900 font-semibold px-4 lg:mt-3">
               <h3 className="lg:absolute">
                 Select from the various services mentioned
               </h3>
@@ -26,7 +26,7 @@ const Works = () => {
             <div className="bg-[#072960] text-white px-6 py-4 rounded-full lg:h-full h-1/2  mx-4">
               <h1 className="font-bold">2</h1>
             </div>
-            <div className="text-blue-900 font-semibold px-4">
+            <div className="text-blue-900 font-semibold px-4 lg:mt-3">
               <h3 className="lg:absolute">
                 Connect us through phone number, whatsapp or email Id
               </h3>
@@ -36,7 +36,7 @@ const Works = () => {
             <div className="bg-[#072960] text-white px-6 py-4 lg:h-full h-1/2  rounded-full mx-4">
               <h1 className="font-bold">3</h1>
             </div>
-            <div className="text-blue-900 font-semibold px-4">
+            <div className="text-blue-900 font-semibold px-4 lg:mt-3">
               <h3 className="lg:absolute">
                 Will provide the best suitable services and solutions
               </h3>
