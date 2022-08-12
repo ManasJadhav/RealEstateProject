caee55fc4cd80cd8b390f2a687a1a693ea8657cf
manasjadhav <manasjjadhav@gmail.com>
2022-08-12 19:14:22 +0530

    changed the how it works..

--- components/works.js
+++ components/works.js
@@ -2,7 +2,7 @@ const Works = () => {
   return (
     <div className="my-12">
       <h1 className="text-4xl text-center font-navbar font-semibold ">
-        how work..
+        how it works...
       </h1>
       <div className="lg:grid lg:grid-cols-2">
         <div>
@@ -18,36 +18,27 @@ const Works = () => {
             </div>
             <div className="text-blue-900 font-semibold px-4 ">
               <h3 className="lg:absolute">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat
+                Select from the various services mentioned
               </h3>
             </div>
           </div>
           <div className="flex lg:my-12 my-20">
             <div className="bg-[#072960] text-white px-6 py-4 rounded-full lg:h-full h-1/2  mx-4">
-              <h1 className="font-bold">1</h1>
+              <h1 className="font-bold">2</h1>
             </div>
             <div className="text-blue-900 font-semibold px-4">
               <h3 className="lg:absolute">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat
+                Connect us through phone number, whatsapp or email Id
               </h3>
             </div>
           </div>
           <div className="flex my-12">
             <div className="bg-[#072960] text-white px-6 py-4 lg:h-full h-1/2  rounded-full mx-4">
-              <h1 className="font-bold">1</h1>
+              <h1 className="font-bold">3</h1>
             </div>
             <div className="text-blue-900 font-semibold px-4">
               <h3 className="lg:absolute">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat
+                Will provide the best suitable services and solutions
               </h3>
             </div>
           </div>
